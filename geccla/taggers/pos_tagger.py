54a914f82d2484eb517e501e250a9640d8788af7
snukky <snk987@gmail.com>
2015-04-09 14:33:49 +0200

    Clean temporary files in POS-tagger.

--- geccla/taggers/pos_tagger.py
+++ geccla/taggers/pos_tagger.py
@@ -6,16 +6,17 @@ import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
 import config
+import cmd
 
 from logger import log
 
 
 class StanfordPOSTagger():
 
-    def __init__(self, threads=8):
+    def __init__(self, threads=None):
         self.tagger_dir = config.TOOLS.STANFORD_TAGGER_DIR
         self.separator = '|'
-        self.threads = threads
+        self.threads = threads or config.THREADS
 
     def tag_file(self, tok_file, pos_file=None, lazy=True):
         if pos_file is None:
@@ -28,16 +29,18 @@ class StanfordPOSTagger():
             
         log.info("tagging file {} into {}".format(tok_file, pos_file))
 
-        cmd = "java -mx1025m -cp {0}/stanford-postagger.jar: " \
+        command = "java -mx1025m -cp {0}/stanford-postagger.jar: " \
             "edu.stanford.nlp.tagger.maxent.MaxentTagger " \
             "-model {0}/models/english-left3words-distsim.tagger " \
             "-sentenceDelimiter newline -tokenize false -tagSeparator \"{3}\" " \
             "-textFile {1} -nthreads {4} 2> /dev/null > {2}" \
             .format(self.tagger_dir, tok_file, ann_file, self.separator, 
                     self.threads)
-        os.popen(cmd)
+        cmd.run(command)
 
-        return self.__extract_pos_tags_in_file(ann_file, pos_file)
+        self.__extract_pos_tags_in_file(ann_file, pos_file)
+        os.remove(ann_file)
+        return pos_file
 
     def __extract_pos_tags_in_file(self, ann_file, pos_file):
         log.debug("extracting tags from file {} into {}".format(ann_file, pos_file))
